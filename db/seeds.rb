6ba9dd6b8df2c3e2c4867d8b9a238b1c3c7e9321
Arzumy MD <hello@ar.zu.my>
2011-05-20 12:14:20 +0800

    moved user import to db/seeds, removed sucky js

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+require "csv"
+CSV.foreach('public/uploads/dumplovr.csv') do |row|
+  users = User.new(
+                 :name          => row[0],
+                 :email         => row[1],
+                 :website       => row[2],
+                 :twitter       => row[3],
+                 :scope         => row[4],
+                 :experience    => row[5],
+                 :level         => row[6],
+                 :designation   => row[7],
+                 :skillset      => row[8],
+                 :other         => row[9],
+                 :recommended   => row[10],
+                 :rates_attributes => {
+                   '0' => {
+                     :currency  => row[11],
+                     :rate      => row[12],
+                     :based     => row[13]
+                   }
+                 }
+               )
+  users.save!
+end
